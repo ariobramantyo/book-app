2ec9d3ecbb58f1bd235e63c5706be4edc4f6758b
Ario Bagus Bramantyo <i.ario.bramantyo@gmail.com>
2023-02-02 11:21:21 +0700

    Add abstraction class for BookRepository

--- packages/book_repository/lib/src/book_repository.dart
+++ packages/book_repository/lib/src/book_repository.dart
@@ -2,17 +2,44 @@ import 'package:api_services/book/book_api_service.dart';
 import 'package:api_services/book/model/book.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-class BookRepository {
+abstract class BookRepository {
+  Future<List<Book>> getBooks();
+  Future<void> addBooks(
+      String isbn,
+      String title,
+      String subtitle,
+      String author,
+      String published,
+      String publisher,
+      String pages,
+      String description,
+      String website);
+  Future<void> deleteBook(int bookId);
+  Future<void> updateBook(
+      int bookId,
+      String isbn,
+      String title,
+      String subtitle,
+      String author,
+      String published,
+      String publisher,
+      String pages,
+      String description,
+      String website);
+}
+
+class BookRepositoryImpl extends BookRepository {
   final BookApiService _bookApiService;
   final FlutterSecureStorage _flutterSecureStorage;
 
-  BookRepository(
+  BookRepositoryImpl(
       {BookApiService? bookApiService,
       FlutterSecureStorage? flutterSecureStorage})
       : _bookApiService = bookApiService ?? BookApiService(),
         _flutterSecureStorage =
             flutterSecureStorage ?? const FlutterSecureStorage();
 
+  @override
   Future<List<Book>> getBooks() async {
     final token = await _flutterSecureStorage.read(key: 'TOKEN');
 
@@ -21,6 +48,7 @@ class BookRepository {
     return books;
   }
 
+  @override
   Future<void> addBooks(
       String isbn,
       String title,
@@ -37,12 +65,14 @@ class BookRepository {
         published, publisher, pages, description, website);
   }
 
+  @override
   Future<void> deleteBook(int bookId) async {
     final token = await _flutterSecureStorage.read(key: 'TOKEN');
 
     await _bookApiService.deleteBook(token!, bookId);
   }
 
+  @override
   Future<void> updateBook(
       int bookId,
       String isbn,
